da7a44238115c2cf7583fc65b87cb01087b6b552
yug jangra <yugjangra226@gmail.com>
2026-07-07 23:21:12 +0530

    feature: secured balance using encapsulation

--- BankAccount.java
+++ BankAccount.java
@@ -1,11 +1,21 @@
 import java.util.Scanner;
-public class BankAccount{
-     double balance;
+class BankAccount{
+     private double balance;
+     public double getBalance(){
+        return balance;
+     }
+     public void setBalance(double balance){
+        this.balance = balance;
+     }
+}
     public void deposit(double amount){
-        balance = balance + amount;
+        BankAccount BA = new BankAccount();
+
+         BA.setBalance(balance + amount);
     }
     public void withdraw(double amount){
-        balance -= amount;
+        BankAccount BA = new BankAccount();
+        BA.setBalance(balance - amount);
     }
     public static void main(String[] args){
         double amount;
@@ -15,21 +25,20 @@ public class BankAccount{
         System.out.println("withdraw or deposit or check balance(cb)");
         String choice = sc.nextLine();
         if(choice.equals("cb")){
-            System.out.println("Balance:" + BA.balance); 
+            System.out.println("Balance:" + BA.getBalance()); 
         }
         else if(choice.equals ("deposit")){
         System.out.println("Enter amount to deposit: ");
         amount = sc.nextDouble();
         sc.nextLine();
         BA.deposit(amount);
-        System.out.println("Balance: " + BA.balance);
+        System.out.println("Balance: " + BA.getBalance());
         }
         else if(choice.equals("withdraw")){
         System.out.println("Enter amount to withdraw: ");
         amount = sc.nextDouble();
         sc.nextLine();
         BA.withdraw(amount);
-        System.out.println("Balance:" + BA.balance);
         }
         else{
             System.out.println("Invalid Choice");
@@ -53,3 +62,5 @@ public class BankAccount{
     sc.close();
 }
 }
+//Encapsulation - accessing private data using methods with the help of getters and setters
+//Made the variable(balance) private 
